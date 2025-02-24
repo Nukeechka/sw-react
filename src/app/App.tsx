b55d2e6a3b1427816f6b292e1227c696576184ab
Nukeechka <nukeechka@gmail.com>
2025-02-24 23:45:16 +0500

    Add signin page (just ui)

---
+++ src/app/App.tsx
@@ -0,0 +1,8 @@
+import { Signin } from "../pages/sign-in";
+import "./App.css";
+
+function App() {
+	return <Signin />;
+}
+
+export default App;
---
+++ src/pages/sign-in/ui/SignIn.tsx
@@ -0,0 +1,28 @@
+import {
+	Button,
+	Card,
+	Elevation,
+	FormGroup,
+	H4,
+	InputGroup,
+} from "@blueprintjs/core";
+import { FC } from "react";
+
+export const Signin: FC = () => {
+	return (
+		<Card elevation={Elevation.ONE}>
+			<H4>Sign in</H4>
+			<FormGroup labelFor="login">
+				<InputGroup id="login" type="text" placeholder="Login" />
+			</FormGroup>
+			<FormGroup labelFor="password">
+				<InputGroup
+					id="password"
+					type="password"
+					placeholder="Password"
+				/>
+			</FormGroup>
+			<Button intent="primary">Sign in</Button>
+		</Card>
+	);
+};
